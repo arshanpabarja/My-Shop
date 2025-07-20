b89458c63fbb922630e030a16ac31cc99c307c90
Arshan Pabarja <pabarjaarshan88@gmail.com>
2025-07-20 23:56:52 +0330

    Update api.jsx

--- Frontend/shop/service/api.jsx
+++ Frontend/shop/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/"
+  baseURL: "https://my-shop-041r.onrender.com/api/"
 })
 
 export async function getProducts() {
